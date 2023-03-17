8db3c6ebf7f4c885c9bd1035d51840dcba48ff8c
Haneefe <haneefalikhan18@gmail.com>
2023-03-17 19:52:25 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import ChannelDetail from "./ChannelDetail";
+import Feed from "./Feed";
+import NavBar from "./NavBar";
+import SearchFeed from "./SearchFeed";
+import VideoDetail from "./VideoDetail";
+
+export{ChannelDetail,Feed,NavBar,SearchFeed,VideoDetail}
